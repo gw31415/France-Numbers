8ac4d54e6040e47b83fb2f69b5efd357311ad32d
Amadeus_vn <git@amas.dev>
2021-08-23 23:03:04 +0900

    Initial Commit

---
+++ France-Numbers/France_NumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  France_NumbersApp.swift
+//  France-Numbers
+//
+//  Created by Yuki on 2021/08/23.
+//
+
+import SwiftUI
+
+@main
+struct France_NumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
